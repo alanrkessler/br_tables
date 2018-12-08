1b7b6293b6c506238ece45391e2019152557ffab
alanrkessler <alan.r.kessler@gmail.com>
2018-12-08 15:49:54 -0600

    Clean up and switch from lxml to html.parser for greater compatibility.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # br_tables
 
-The ability to scrape [Baseball Reference](https://www.baseball-reference.com/) tables to create pandas DataFrames. The website is a great resource for a wide variety of statistics. This is a rewrite of the many of the outdated and broken scripts to do this scraping found around the web. Specifically, the newer elements of the Baseball Reference website create require different processing.
+Provides the ability to scrape [Baseball Reference](https://www.baseball-reference.com/) tables to create pandas DataFrames. This is a rewrite of the many of the outdated and broken scripts to do this scraping found around the web. Specifically, the newer elements of the Baseball Reference website create require different processing.
 
 Please follow Baseball Reference's terms of service associated with collecting and using the data they host.
 
--- br.py
+++ br.py
@@ -1,47 +1,59 @@
-from bs4 import BeautifulSoup
+# -*- coding: utf-8 -*-
+"""Access Baseball Reference Tables.
+
+Delivers tables in a dictionary that can be manipulated in pandas or numpy.
+"""
+
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 
 def table_criteria(tag):
     """Criteria for stats tables for find_all"""
+    table_tag = None
     if not tag.has_attr("class"):
-        return False
+        table_tag = False
     else:
-        return (tag.name == "table" and
-                "stats_table" in tag["class"] and
-                "sortable" in tag["class"])
+        table_tag = (tag.name == "table" and
+                     "stats_table" in tag["class"] and
+                     "sortable" in tag["class"])
+    return table_tag
 
 
 def row_criteria(tag):
     """Criteria for row in stats table for find_all"""
+    row_tag = None
     if not tag.name == "tr":
-        return False
-    if not tag.has_attr("class"):
-        return True
-    return ("league_average_table" not in tag["class"] and
-            "stat_total" not in tag["class"] and
-            "thead" not in tag['class'])
+        row_tag = False
+    elif not tag.has_attr("class"):
+        row_tag = True
+    else:
+        row_tag = ("league_average_table" not in tag["class"] and
+                   "stat_total" not in tag["class"] and
+                   "thead" not in tag['class'])
+    return row_tag
 
 
 def tables(url):
-    """Return all tables from baseball reference as dictionary"""
-    dataDict = {}
+    """Return all tables from a baseball reference url as dictionary"""
+    data_dict = {}
 
-    # Tables easily accessible
-    soup = BeautifulSoup(urlopen(url), "lxml")
-    tables = soup.find_all(table_criteria)
+    # List tables that are easily accessible
+    soup = BeautifulSoup(urlopen(url), 'html.parser')
+    tables_list = soup.find_all(table_criteria)
 
-    # Tables within comments to fill out list of tables
+    # List tables that are within comments
     comments = soup.find_all('div', class_='placeholder')
     for comment in comments:
         try:
-            sub_soup = BeautifulSoup(comment.next_sibling.next_sibling, 'lxml')
+            soup = BeautifulSoup(comment.next_sibling.next_sibling,
+                                 'html.parser')
         except TypeError:
             pass
-        tables.extend(sub_soup.find_all(table_criteria))
+        tables_list.extend(soup.find_all(table_criteria))
 
     # Add each table to the dictionary
-    for table in tables:
+    for table in tables_list:
         # Key is the table caption
         caption = table.find('caption').text
 
@@ -63,6 +75,6 @@ def tables(url):
             cols = [ele.text.strip() for ele in cols]
             body.append([ele for ele in cols])
 
-        dataDict[caption] = [header[0], body]
+        data_dict[caption] = [header[0], body]
 
-    return dataDict
+    return data_dict
